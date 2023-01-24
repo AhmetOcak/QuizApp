800be7ea0bdbe805d74f450770f802979a6e796d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-01-25 00:05:29 +0300

    new types added

--- app/src/main/java/com/quizapp/core/ui/theme/Type.kt
+++ app/src/main/java/com/quizapp/core/ui/theme/Type.kt
@@ -11,42 +11,45 @@ val Typography = Typography(
     h1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 30.sp,
-        letterSpacing = 1.15.sp
+        fontSize = 30.sp
     ),
     h2 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
-        letterSpacing = 1.15.sp
     ),
     h3 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
-        letterSpacing = 1.15.sp
+        fontSize = 18.sp
     ),
     h4 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        letterSpacing = 1.15.sp
+        fontSize = 16.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
     ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
-        letterSpacing = 0.sp
+        fontSize = 22.sp
     ),
     body2 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        letterSpacing = 1.sp
-    ) ,
+        fontSize = 18.sp
+    ),
     button = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        letterSpacing = 1.15.sp
+        fontWeight = FontWeight.Bold
     )
 )
