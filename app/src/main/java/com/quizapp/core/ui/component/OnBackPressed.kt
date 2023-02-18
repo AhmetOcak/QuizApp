5cc1fc0689f39194c2750753dafaf2d37a63a1fb
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-02-18 21:00:35 +0300

    new func added for go back screen

--- app/src/main/java/com/quizapp/core/ui/component/OnBackPressed.kt
+++ app/src/main/java/com/quizapp/core/ui/component/OnBackPressed.kt
@@ -3,11 +3,21 @@ package com.quizapp.core.ui.component
 import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import com.quizapp.core.navigation.Navigator
 
 // Created for bottom navigation screens
 @Composable
 fun OnBackPressed(activity: Activity) {
     BackHandler {
         activity.finish()
+    }
+}
+
+// Created for normal screens
+// Navigate back screen
+@Composable
+fun OnBackPressed(targetRoute: String) {
+    BackHandler() {
+        Navigator.navigate(destination = targetRoute) {}
     }
 }
