e5d423a31ca561bc826004abdf469f25afa28626
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-03-03 22:38:20 +0300

    helper class created for pass object as nav arg

---
+++ app/src/main/java/com/quizapp/core/common/JsonNavType.kt
@@ -0,0 +1,17 @@
+package com.quizapp.core.common
+
+import android.os.Bundle
+import androidx.navigation.NavType
+
+abstract class JsonNavType<T> : NavType<T>(isNullableAllowed = false) {
+    abstract fun fromJsonParse(value: String): T
+    abstract fun T.getJsonParse(): String
+
+    override fun get(bundle: Bundle, key: String): T? =
+        bundle.getString(key)?.let { parseValue(it) }
+
+    override fun parseValue(value: String): T = fromJsonParse(value)
+
+    override fun put(bundle: Bundle, key: String, value: T) =
+        bundle.putString(key, value.getJsonParse())
+}
