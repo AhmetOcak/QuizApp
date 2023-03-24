72eaa585d57e45df46026e5b4ca19e596f2391fa
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-03-24 21:55:33 +0300

    unused files removed

--- app/src/main/java/com/quizapp/data/datasource/remote/options/entity/CreateOptionsDto.kt
+++
@@ -1,7 +0,0 @@
-package com.quizapp.data.datasource.remote.options.entity
-
-data class CreateOptionsDto(
-    val questionId: String,
-    val description: String,
-    val isAnswer: Boolean
-)
--- app/src/main/java/com/quizapp/data/datasource/remote/question/entity/CreateQuestionDto.kt
+++
@@ -1,7 +0,0 @@
-package com.quizapp.data.datasource.remote.question.entity
-
-data class CreateQuestionDto(
-    val quizId: String,
-    val title: String,
-    val description: String
-)
