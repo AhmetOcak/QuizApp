5ad27d6f5c4ce3f1dd09f670b4ab9b1f19e6874b
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-04-08 14:43:56 +0300

    expired token handled

--- app/src/main/java/com/quizapp/domain/usecase/user/GetUserProfileUseCase.kt
+++ app/src/main/java/com/quizapp/domain/usecase/user/GetUserProfileUseCase.kt
@@ -1,8 +1,11 @@
 package com.quizapp.domain.usecase.user
 
+import android.content.SharedPreferences
 import android.util.Log
 import com.quizapp.core.common.Response
 import com.quizapp.core.common.getErrorMessage
+import com.quizapp.core.navigation.NavScreen
+import com.quizapp.core.navigation.Navigator
 import com.quizapp.domain.model.user.UserProfile
 import com.quizapp.domain.repository.UserRepository
 import com.quizapp.domain.utils.Messages
@@ -12,9 +15,11 @@ import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
-class GetUserProfileUseCase @Inject constructor(private val repository: UserRepository) {
-
-    suspend operator fun invoke(token: String) : Flow<Response<UserProfile>> = flow {
+class GetUserProfileUseCase @Inject constructor(
+    private val repository: UserRepository,
+    private val sharedPreferences: SharedPreferences
+) {
+    suspend operator fun invoke(token: String): Flow<Response<UserProfile>> = flow {
         try {
             emit(Response.Loading)
 
@@ -23,13 +28,19 @@ class GetUserProfileUseCase @Inject constructor(private val repository: UserRepo
             emit(Response.Error(errorMessage = Messages.INTERNET))
             Log.e("GetUserProfileUseCase.kt", e.stackTraceToString())
         } catch (e: HttpException) {
-            val errorMessage = e.getErrorMessage()
-            if (errorMessage != null) {
-                emit(Response.Error(errorMessage = errorMessage))
+            if (e.code() == 401) {
+                Navigator.navigate(NavScreen.SignInScreen.route) {
+                    popUpTo(0)
+                }
             } else {
-                emit(Response.Error(errorMessage = Messages.UNKNOWN))
+                val errorMessage = e.getErrorMessage()
+                if (errorMessage != null) {
+                    emit(Response.Error(errorMessage = errorMessage))
+                } else {
+                    emit(Response.Error(errorMessage = Messages.UNKNOWN))
+                }
+                Log.e("GetUserProfileUseCase.kt", e.stackTraceToString())
             }
-            Log.e("GetUserProfileUseCase.kt", e.stackTraceToString())
         } catch (e: Exception) {
             emit(Response.Error(errorMessage = e.message ?: Messages.UNKNOWN))
             Log.e("GetUserProfileUseCase.kt", e.stackTraceToString())
